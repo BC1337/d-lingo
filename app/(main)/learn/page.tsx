1110603da90886f1358b5ce718c47f6ea4c79659
BC1337 <brettcomrie@gmail.com>
2024-03-31 08:50:47 -0500

    alot of UI update shenanigans, saving before moving forward

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { StickyWrapper } from "@/components/ui/sticky-wrapper"
+import { FeedWrapper } from "@/components/ui/feed-wrapper"
+import { Header } from "./header"
+import { UserProgress } from "@/components/ui/user-progress"
 
 const Learn = () => {
   return (
-    <div>Learn</div>
+    // todo: try and remove flex-row-reverse and move feed-wrapper ontop of stickyWrapper for the same functionality but more clean code
+    <div className='flex flex-row-reverse gap-[48px] px-6'>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg"}}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish"/>
+      </FeedWrapper>
+    </div>
   )
 }
 
